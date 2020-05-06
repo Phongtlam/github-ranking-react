7c6839d518c46f7b893bf02c830aec20bb9e173b
Phongtlam <Phongtlam@gmail.com>
2020-05-06 00:41:18 -0700

    Updates

---
+++ precache-manifest.64e36de15bcbf86230265c731ef38874.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68baf9c719555a4d4605e73da1179f08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ac9048cdf699b0c0b5c",
+    "url": "/static/css/2.ed08051a.chunk.css"
+  },
+  {
+    "revision": "cb6a259854ec64e62a9e",
+    "url": "/static/css/main.c26cdb89.chunk.css"
+  },
+  {
+    "revision": "8ac9048cdf699b0c0b5c",
+    "url": "/static/js/2.3b16981c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b16981c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6a259854ec64e62a9e",
+    "url": "/static/js/main.fce8feb4.chunk.js"
+  },
+  {
+    "revision": "359d2f3ee6446665373f",
+    "url": "/static/js/runtime-main.040fb519.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
